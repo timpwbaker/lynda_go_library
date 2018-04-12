2b98ef625a1daa1adc359171bf35dbd876de3804
Tim Baker <timpwbaker@gmail.com>
2018-04-12 07:34:31 +0100

    End of lesson 1

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Page struct {
+	Name     string
+	DBstatus bool
+}
+
+func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	db, _ := sql.Open("sqlite3", "dev.db")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		p := Page{
+			Name: "Gopher",
+		}
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+		p.DBstatus = db.Ping() == nil
+
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
